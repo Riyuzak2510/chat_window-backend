0f725ae797155a481ba168191702e8d3a6dda6f1
Mayank Rastogi <mayank251097@gmail.com>
2019-01-19 18:27:08 +0530

    MY_CHAT
    Signed-off-by: Mayank Rastogi <mayank251097@gmail.com>

--- socket/socket.js
+++ socket/socket.js
@@ -10,4 +10,36 @@ module.exports = function(io,rooms){
             socket.emit('roomupdate',JSON.stringify(rooms))
         })
     })
+    var messages = io.of('/messages').on('connection',function(socket){
+        console.log("Connection Done!!")
+        socket.on('joinroom',function(data){
+            socket.username = data.username
+            socket.userPic = data.userPic
+            socket.join(data.room)
+            updateUserList(data.room,true)
+        })
+
+        socket.on('newmessage',function(data){
+            socket.broadcast.to(data.room_number).emit('messagefeed',JSON.stringify(data))
+        })
+
+        function updateUserList(room,updateALL)
+        {
+            var getusers = io.of('/messages').in(room).clients
+            var userlist = []
+            for(var i in getusers)
+            {
+                userlist.push({user:getusers[i].username})
+                console.log(getusers[i].username)
+            }
+            socket.to(room).emit('updateUsersList',JSON.stringify(userlist))
+            if(updateALL) {
+                socket.broadcast.to(room).emit('updateUsersList')
+            }
+        }
+
+        socket.on('updateList',function(data){
+            updateUserList(data.room)
+        })
+    })
 }
