cb5c1e93e9702f2d75f649054356c2609bf9b58a
Mayank Rastogi <mayank251097@gmail.com>
2019-01-19 20:10:57 +0530

    MY_CHAT
    Signed-off-by: Mayank Rastogi <mayank251097@gmail.com>

--- socket/socket.js
+++ socket/socket.js
@@ -2,7 +2,7 @@ module.exports = function(io,rooms){
     var chatrooms = io.of('/roomlist').on('connection',function(socket){
         console.log("Connection Done!")
         socket.on('getrooms',function(){
-            socket.emit('printrooms',JSON.stringify(rooms))
+        socket.emit('printrooms',JSON.stringify(rooms))
         })
         socket.on('newRoom',function(data){
             rooms.push(data)
@@ -23,23 +23,27 @@ module.exports = function(io,rooms){
             socket.broadcast.to(data.room_number).emit('messagefeed',JSON.stringify(data))
         })
 
-        function updateUserList(room,updateALL)
-        {
-            var getusers = io.of('/messages').in(room).clients
+        function updateUserList(room,updateALL){
+            var getusers
+            io.of('/messages').in(room).clients((error, clients) => {
+                if (error) throw error;
+                getusers = clients
+           });
+           
             var userlist = []
             for(var i in getusers)
             {
                 userlist.push({user:getusers[i].username})
-                console.log(getusers[i].username)
             }
+            //console.log(userlist.length)
             socket.to(room).emit('updateUsersList',JSON.stringify(userlist))
             if(updateALL) {
-                socket.broadcast.to(room).emit('updateUsersList')
+                socket.broadcast.to(room).emit('updateUsersList',JSON.stringify(userlist))
             }
         }
 
         socket.on('updateList',function(data){
-            updateUserList(data.room)
+            updateUserList(data.room,false)
         })
     })
 }
